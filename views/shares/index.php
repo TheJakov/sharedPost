82872717e078a392243fa1e63e920707c2deaef7
TheJakov <kristovic.jakov@gmail.com>
2020-02-09 00:05:58 +0100

    Stylished shares/index.php layout

--- views/shares/index.php
+++ views/shares/index.php
@@ -1,1 +1,21 @@
-THIS IS SHARES/INDEX
+<div class="sharesMainDiv">
+    <a class="btn btn-success btn-share" href="<?php echo ROOT_PATH; ?>shares/add">Share Something</a>
+    
+        <?php foreach($viewmodel as $item): ?>
+            <div class="card">
+                <div class="card-header custom-card-class">
+                    <?php echo $item['title']; ?>
+                    <small><?php echo $item['create_date']; ?></small>
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <h6><?php echo $item['body']; ?></h6>
+                        <div class="custom-card-class">
+                            <footer class="blockquote-footer">From anonymous user</footer>
+                            <a class="btn btn-secondary" href="<?php echo $item['link']; ?>" target="_blank">Find out more</a>
+                        </div>
+                    </blockquote>
+                </div>
+            </div>
+        <?php endforeach; ?>
+</div>
