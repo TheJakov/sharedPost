42aec740eaf9463c7b947c061085c48c31647968
TheJakov <kristovic.jakov@gmail.com>
2020-02-09 03:10:42 +0100

    Enabled functionality of adding new Shared posts

--- classes/Model.php
+++ classes/Model.php
@@ -40,6 +40,10 @@ abstract class Model{
         $this->execute();
         return $this->statement->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function lastInsertId(){
+        return $this->dbh->lastInsertId();
+    }
 }
 
 ?>
--- controllers/shares.php
+++ controllers/shares.php
@@ -1,7 +1,13 @@
 <?php
 class Shares extends Controller{
+
 	protected function Index(){
 		$viewModel = new ShareModel();
 		$this->returnView($viewModel->Index(), true);
+	}
+
+	protected function add(){
+		$viewModel = new ShareModel();
+		$this->returnView($viewModel->add(), true);
 	}
 }
